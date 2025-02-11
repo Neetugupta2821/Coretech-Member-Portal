a7e8de8c5569c70d1b318a8574a0d81286a3b406
nehavishvas <104732802+nehavishvas@users.noreply.github.com>
2025-02-11 16:54:43 +0530

    Added new feature/fix

--- src/main.js
+++ src/main.js
@@ -2,7 +2,6 @@ import { createApp } from 'vue';
 import App from './App.vue';
 import router from './router';
 import store from './store';
-
 import Aura from '@primevue/themes/aura';
 import PrimeVue from 'primevue/config';
 import ConfirmationService from 'primevue/confirmationservice';
